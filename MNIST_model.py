9b1ca87242410444812255c75a39b49027ac5551
MindmasterTrainerChristian <christian.leininger@logiscool.com>
2026-03-23 18:32:04 +0100

    Implement data loading and result plotting for MNIST
    Added data loading for MNIST dataset and a function to plot results.

--- MNIST_model.py
+++ MNIST_model.py
@@ -3,3 +3,22 @@ import torch.nn as nn
 import torch.optim as optim
 from torchvision import datasets, transforms
 import matplotlib.pyplot as plt
+
+
+# Data Loading
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+train_loader = torch.utils.data.DataLoader(
+    datasets.MNIST('./data', train=True, download=True, transform=transform),
+    batch_size=64, shuffle=True)
+
+
+def plot_results(data, target ):
+    pred = [0] * target.shape[0]
+    print(pred)
+    plt.figure(figsize=(12, 4))
+    for i in range(8):
+        plt.subplot(1, 8, i+1)
+        plt.imshow(data[i].squeeze(), cmap='gray')
+        plt.title(f"Pred: {pred[i]}\nActual: {target[i]}")
+        plt.axis('off')
+    plt.show()
