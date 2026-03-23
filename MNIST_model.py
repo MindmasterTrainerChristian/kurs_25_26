446d109f621fd120fbf491ea166d0b02e7933a9c
MindmasterTrainerChristian <christian.leininger@logiscool.com>
2026-03-23 18:50:50 +0100

    Add SimpleNetwork class for MNIST model

--- MNIST_model.py
+++ MNIST_model.py
@@ -22,3 +22,19 @@ def plot_results(data, target ):
         plt.title(f"Pred: {pred[i]}\nActual: {target[i]}")
         plt.axis('off')
     plt.show()
+
+
+class SimpleNetwork(torch.nn.Module):
+    def __init__(self):
+        super(SimpleNetwork, self).__init__()
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(28*28, 128)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.fc2(x)
+        return x
